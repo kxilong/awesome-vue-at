128d738dd3ac4402e8c7671c36ba59b2d72bb5de
kxilong <44635940+kxilong@users.noreply.github.com>
2022-06-21 08:10:46 +0000

    Deploying to gh-pages from @ kxilong/awesome-vue-at@6747cf3626b2819bdff8881b652ab54e451e4202 🚀

--- vite/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import vue from "@vitejs/plugin-vue";
-
-import createMock from "./mock";
-
-export default function createVitePlugins(viteEnv, isBuild = false) {
-  const vitePlugins = [vue()];
-  vitePlugins.push(createMock());
-  return vitePlugins;
-}
